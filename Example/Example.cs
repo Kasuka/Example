415e4e86042683eb72474ec7908a41e1db001f09
k_ueno <neue_erde@hotmail.com>
2015-02-17 20:01:14 +0900

    DisplayOptions

--- Example/Example.cs
+++ Example/Example.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 
-class BoolDemo
+class DisplayOptions
 {
     static void Main()
     {
-        bool b;
+        int i;
 
-        b = false;
-        Console.WriteLine("b is " + b);
-        b = true;
-        Console.WriteLine("b is " + b);
+        Console.WriteLine("Value\tSquare\tCubed");
 
-        if (b)
-            Console.WriteLine("This is executed.");
-
-        b = false;
-        if (b)
-            Console.WriteLine("This is not executed.");
-
-        Console.WriteLine("10 > 9 is " + (10 > 9));
+        for (i = 1; i < 10; i++)
+        {
+            Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+        }
     }
 }
