a6d65ddd6c8b96418296a2f90fabe41d7d8b9212
k_ueno <neue_erde@hotmail.com>
2015-02-24 21:22:41 +0900

    Implicitly Typed Var

--- Example/Example.cs
+++ Example/Example.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 
-class DynInit
+class ImplicitlyTypedVar
 {
     static void Main()
     {
-        double s1 = 4.0;
-        double s2 = 5.0;
+        var s1 = 4.0;
+        var s2 = 5.0;
 
-        double hypot = Math.Sqrt((s1 * s1) + (s2 * s2));
+        var hypot = Math.Sqrt((s1 * s1) + (s2 * s2));
 
         Console.Write("Hypotenuse of triangle with sides " + s1 + " by " + s2 + " is ");
-
         Console.WriteLine("{0:#.###}", hypot);
     }
 }
