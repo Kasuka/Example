5e2df5167e57d13e77362c156fb8613b59ecc81d
k_ueno <neue_erde@hotmail.com>
2015-03-01 00:01:06 +0900

    CastDemo

--- Example/Example.cs
+++ Example/Example.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 
-class LtoD
+class CastDemo
 {
     static void Main()
     {
-        long L;
-        double D;
+        double x, y;
+        byte b;
+        int i;
+        char ch;
+        uint u;
+        short s;
+        long l;
 
-        L = 100123285L;
-        D = L;
+        x = 10.0;
+        y = 3.0;
 
-        Console.WriteLine("L and D: " + L + " " + D);
+        i = (int)(x / y);
+        Console.WriteLine("Integer outcome of x / y: " + i);
+        Console.WriteLine();
+
+        i = 255;
+        b = (byte)i;
+        Console.WriteLine("b after assigning 255: " + b + " -- no data lost");
+
+        i = 257;
+        b = (byte)i;
+        Console.WriteLine("b after assigning 257: " + b + " -- data lost.");
+        Console.WriteLine();
+
+        u = 32000;
+        s = (short)u;
+        Console.WriteLine("s after assigning 32000: " + s + " -- no data lost.");
+
+        u = 64000;
+        s = (short)u;
+        Console.WriteLine("s after assigning 64000: " + s + " -- data lost.");
+
+        l = -12;
+        u = (uint)l;
+        Console.WriteLine("u after assigning -12: " + u + " -- data lost.");
+        Console.WriteLine();
+
+        b = 88;
+        ch = (char)b;
+        Console.WriteLine("ch after assigning 88: " + ch);
     }
 }
