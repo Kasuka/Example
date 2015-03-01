16e1ca93b676c95ad2815a7a882a9e2164108ab1
k_ueno <neue_erde@hotmail.com>
2015-03-01 11:15:02 +0900

    PromDemo

--- Example/Example.cs
+++ Example/Example.cs
@@ -1,52 +1,14 @@
 ﻿using System;
 
-class CastDemo
+class PromDemo
 {
     static void Main()
     {
-        double x, y;
         byte b;
-        int i;
-        char ch;
-        uint u;
-        short s;
-        long l;
 
-        x = 10.0;
-        y = 3.0;
+        b = 10;
+        b = (byte)(b * b);
 
-        i = (int)(x / y);
-        Console.WriteLine("Integer outcome of x / y: " + i);
-        Console.WriteLine();
-
-        i = 255;
-        b = (byte)i;
-        Console.WriteLine("b after assigning 255: " + b + " -- no data lost");
-
-        i = 257;
-        b = (byte)i;
-        Console.WriteLine("b after assigning 257: " + b + " -- data lost.");
-        Console.WriteLine();
-
-        u = 32000;
-        s = (short)u;
-        Console.WriteLine("s after assigning 32000: " + s + " -- no data lost.");
-
-        u = 64000;
-        s = (short)u;
-        Console.WriteLine("s after assigning 64000: " + s + " -- data lost.");
-
-        l = 64000;
-        u = (uint)l;
-        Console.WriteLine("u after assigning 64000: " + u + " -- no data lost.");
-
-        l = -12;
-        u = (uint)l;
-        Console.WriteLine("u after assigning -12: " + u + " -- data lost.");
-        Console.WriteLine();
-
-        b = 88;
-        ch = (char)b;
-        Console.WriteLine("ch after assigning 88: " + ch);
+        Console.WriteLine("b: " + b);
     }
 }
